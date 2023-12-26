a950668dbd87a90519cdcfdcbd5015fa5e851ea7
Dada Khalandar <kerry.blig12@gmail.com>
2023-12-27 04:53:07 +0800

    added the tabs

--- container/planets-container/planets-container.tsx
+++ container/planets-container/planets-container.tsx
@@ -2,30 +2,45 @@
 
 import { planetsData } from '@/utils/JsonData';
 import Image from 'next/image';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { PlanetWeatherCard } from '@/components/card/planet-weather-card';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 interface PlanetsContainerProps {
   planetName: string;
 }
 
 export const PlanetsContainer: FC<PlanetsContainerProps> = ({ planetName }) => {
-  const selectedPlanet = planetsData?.find(
+  // states
+  const [isInternal, setIsInternal] = useState<string>('planet');
+  const [geology, setGeology] = useState<string>('');
+  const selectedPlanet: any = planetsData?.find(
     (planet) => planet?.name.toLowerCase() === planetName
   );
-  console.log('selectedPlanet:', selectedPlanet);
+
   return (
     <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
       <div className=' flex flex-col md:flex-row justify-around items-center md:items-start my-8'>
-        <div className='w-[18rem] h-[18rem] '>
+        <div className='w-[19rem] h-[19rem] '>
           <Image
-            src={selectedPlanet?.images?.planet as string}
+            src={selectedPlanet?.images?.[isInternal]}
             width={200}
             height={200}
-            objectFit='cover'
+            // objectFit='cover'
             priority={true}
             alt={selectedPlanet?.name as string}
           />
+          {geology && (
+            <Image
+              src={selectedPlanet?.images?.[geology]}
+              width={100}
+              height={100}
+              // objectFit='cover'
+              priority={true}
+              className='relative bottom-12 left-6 flex justify-center w-[10rem] h-[10rem] '
+              alt={selectedPlanet?.name as string}
+            />
+          )}
         </div>
         <div className='max-w-[21rem] text-center md:text-left justify-end'>
           <div>{selectedPlanet?.name}</div>
@@ -40,6 +55,38 @@ export const PlanetsContainer: FC<PlanetsContainerProps> = ({ planetName }) => {
               {selectedPlanet?.overview.source}
             </a>
           </div>
+          <Tabs defaultValue='account' className='w-[400px]'>
+            <TabsList className='flex flex-col    h-[200px] bg-transparent'>
+              <TabsTrigger
+                value='account'
+                className='w-[10rem]'
+                onClick={() => {
+                  setIsInternal('planet');
+                  setGeology('');
+                }}
+              >
+                Account
+              </TabsTrigger>
+              <TabsTrigger
+                value='password'
+                onClick={() => {
+                  setIsInternal('internal');
+                  setGeology('');
+                }}
+              >
+                Internal
+              </TabsTrigger>
+              <TabsTrigger
+                value='password1'
+                onClick={() => {
+                  setIsInternal('planet');
+                  setGeology('geology');
+                }}
+              >
+                Password1
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
         </div>
       </div>
       <div className='flex  flex-wrap justify-center mt-8 items-start gap-10'>
