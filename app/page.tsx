03af40624e1bfbd40be48708756e08352b4ace62
Dada Khalandar <kerry.blig12@gmail.com>
2023-12-27 01:53:09 +0800

    testing images

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import { planetsData } from '@/utils/JsonData';
+import Image from 'next/image';
+
 export default function Home() {
-  return <div>dfg</div>;
+  return (
+    <div>
+      {planetsData?.map((planet) => (
+        <div key={planet?.name}>
+          <Image
+            src={planet?.images?.geology}
+            alt={planet?.name}
+            width={150}
+            height={150}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
