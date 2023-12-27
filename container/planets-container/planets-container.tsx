d4421aee7cc5d8a10d90741a6b1ac65ad2218ff4
Dada Khalandar <kerry.blig12@gmail.com>
2023-12-27 15:35:49 +0800

    testing the active class colors

--- container/planets-container/planets-container.tsx
+++ container/planets-container/planets-container.tsx
@@ -5,7 +5,8 @@ import Image from 'next/image';
 import { FC, useState } from 'react';
 import { PlanetWeatherCard } from '@/components/card/planet-weather-card';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
-
+import Link from 'next/link';
+import sourceIcon from '@/public/assets/icon-source.svg';
 interface PlanetsContainerProps {
   planetName: string;
 }
@@ -18,8 +19,24 @@ export const PlanetsContainer: FC<PlanetsContainerProps> = ({ planetName }) => {
     (planet) => planet?.name.toLowerCase() === planetName
   );
 
+  const planetColors = {
+    mercury: 'mercury',
+    venus: 'venus',
+    earth: 'earth-color',
+    mars: 'mars',
+    jupiter: 'jupiter',
+    saturn: 'saturn',
+    uranus: 'uranus',
+    neptune: 'neptune',
+  };
+
+  const activeColorClass =
+    planetColors[planetName as keyof typeof planetColors]; // default color: ;
+  console.log('planetName:', planetName);
+  console.log('activeColorClass:', activeColorClass);
+
   return (
-    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-[8rem]'>
       <div className=' flex flex-col md:flex-row justify-around items-center md:items-start my-8'>
         <div className='w-[19rem] h-[19rem] '>
           <Image
@@ -43,47 +60,58 @@ export const PlanetsContainer: FC<PlanetsContainerProps> = ({ planetName }) => {
           )}
         </div>
         <div className='max-w-[21rem] text-center md:text-left justify-end'>
-          <div>{selectedPlanet?.name}</div>
-          <div>{selectedPlanet?.overview.content}</div>
-          <div>
-            Source:{' '}
-            <a
-              href={selectedPlanet?.overview.source}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {selectedPlanet?.overview.source}
-            </a>
+          <div className='flex flex-col gap-10'>
+            <div className='text-7xl uppercase'>{selectedPlanet?.name}</div>
+            <div className='text-md font-league-spartan break-words font-normal'>
+              {selectedPlanet?.overview.content}
+            </div>
+            <div className='flex gap-2 w-full text-lg font-league-spartan break-words font-normal '>
+              Source:{' '}
+              <Link
+                href={selectedPlanet?.overview.source}
+                target='_blank'
+                className='flex gap-2 items-baseline font-bold opacity-60 leading-6 underline'
+              >
+                Wikipedia
+                <Image
+                  src={sourceIcon}
+                  alt={'sourceIcon'}
+                  width={15}
+                  className='w-[0.75rem] h-[0.75rem] items'
+                />
+              </Link>
+            </div>
           </div>
-          <Tabs defaultValue='account' className='w-[400px]'>
-            <TabsList className='flex flex-col    h-[200px] bg-transparent'>
+
+          <Tabs defaultValue='overview' className='w-[400px]'>
+            <TabsList className='flex flex-col h-[200px] bg-transparent '>
               <TabsTrigger
-                value='account'
-                className='w-[10rem]'
+                value='overview'
+                className={`w-[10rem] data-[state=active]:bg-${activeColorClass} `}
                 onClick={() => {
                   setIsInternal('planet');
                   setGeology('');
                 }}
               >
-                Account
+                Overview
               </TabsTrigger>
               <TabsTrigger
-                value='password'
+                value='interval'
                 onClick={() => {
                   setIsInternal('internal');
                   setGeology('');
                 }}
               >
-                Internal
+                Internal Structure
               </TabsTrigger>
               <TabsTrigger
-                value='password1'
+                value='geology'
                 onClick={() => {
                   setIsInternal('planet');
                   setGeology('geology');
                 }}
               >
-                Password1
+                Surface Geology
               </TabsTrigger>
             </TabsList>
           </Tabs>
